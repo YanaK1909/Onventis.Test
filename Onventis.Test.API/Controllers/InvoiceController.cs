4c38cfa470579f6ae8c5c2ccb3ef3c2a103c9362
Yana Kostrova <kostrovayana@gmail.com>
2022-06-25 19:42:17 +0300

    Add initial web api project

---
+++ Onventis.Test.API/Controllers/InvoiceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Onventis.Test.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly ILogger<InvoiceController> _logger;
+
+        public InvoiceController(ILogger<InvoiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult Approve(int id)
+        {
+            _logger.LogInformation($"Invoice with ID {id} is approved");
+            return Ok();
+        }
+    }
+}
